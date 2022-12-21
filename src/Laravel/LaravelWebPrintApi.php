6cb1e6e46fbaeca06330fbf945ab34c474c0a0a3
Krystian Duma <git@krystian.duma.sh>
2022-12-21 21:29:53 +0100

    Add laravel integration

---
+++ src/Laravel/LaravelWebPrintApi.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace KDuma\WebPrintClient\Laravel;
+
+use KDuma\WebPrintClient\Response\Printer;
+use KDuma\WebPrintClient\Response\Promise;
+use KDuma\WebPrintClient\WebPrintApi;
+use KDuma\WebPrintClient\WebPrintApiInterface;
+
+class LaravelWebPrintApi extends WebPrintApi implements WebPrintApiInterface
+{
+    public function GetPrinter(string $uuid): Printer
+    {
+        return parent::GetPrinter($uuid);
+    }
+
+    public function UpdatePromise(string  $uuid,
+                                  ?string $name = null,
+                                  ?string $printer_uuid = null,
+                                  ?array  $meta = null,
+                                  ?array  $ppd_options = null,
+                                  ?string $status = null): void
+    {
+        $printer_uuid = config(sprintf("webprint.printers.%s", $printer_uuid)) ?? $printer_uuid;
+
+        parent::UpdatePromise($uuid, $name, $printer_uuid, $meta, $ppd_options, $status);
+    }
+
+    public function CreatePromise(string  $name,
+                                  string  $type,
+                                  ?array  $meta = null,
+                                  ?string $printer_uuid = null,
+                                  ?array  $available_printers = null,
+                                  ?array  $ppd_options = null,
+                                  ?string $content = null,
+                                  ?string $file_name = null,
+                                  ?bool   $headless = null): Promise
+    {
+        $printer_uuid = config(sprintf("webprint.printers.%s", $printer_uuid)) ?? $printer_uuid;
+
+        return parent::CreatePromise($name, $type, $meta, $printer_uuid, $available_printers, $ppd_options, $content, $file_name, $headless);
+    }
+
+}
---
+++ src/Laravel/WebPrintClientServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace KDuma\WebPrintClient\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+use KDuma\WebPrintClient\HttpClient\GuzzleHttp7Client;
+use KDuma\WebPrintClient\HttpClient\HttpClientInterface;
+use KDuma\WebPrintClient\WebPrintApi;
+use KDuma\WebPrintClient\WebPrintApiInterface;
+
+class WebPrintClientServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/config.php' => config_path('webprint-client.php'),
+            ], 'config');
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'webprint');
+
+        $this->app->bind(HttpClientInterface::class, fn() => new GuzzleHttp7Client(
+            config('webprint.endpoint'),
+            config('webprint.token'),
+        ));
+
+        $this->app->bind(WebPrintApiInterface::class, LaravelWebPrintApi::class);
+        $this->app->bind(WebPrintApi::class, LaravelWebPrintApi::class);
+    }
+}
